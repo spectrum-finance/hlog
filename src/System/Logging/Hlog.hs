6d4e3d74ef98f80d6d5b509c393ac160aa94bcf3
Bromel777 <alexbromel@gmail.com>
2023-06-14 16:09:49 +0200

    strict cfgs

--- src/System/Logging/Hlog.hs
+++ src/System/Logging/Hlog.hs
@@ -28,9 +28,9 @@ type Format = String
 type Component = String
 
 data LoggingConfig = LoggingConfig
-  { fileHandlers   :: [(FilePath, LogLevel, Format)]
-  , levelOverrides :: [(Component, LogLevel)]
-  , rootLogLevel   :: LogLevel
+  { fileHandlers   :: ![(FilePath, LogLevel, Format)]
+  , levelOverrides :: ![(Component, LogLevel)]
+  , rootLogLevel   :: !LogLevel
   } deriving (Generic, Show, FromDhall)
 
 instance FromDhall (FilePath, LogLevel, Format)
@@ -48,22 +48,13 @@ data Logging f = Logging
   , errorM :: forall a. Loggable a => a -> f ()
   }
 
--- loggingForComponent :: (Applicative f, MonadIO m) => Component -> f (Logging m)
--- loggingForComponent name = do
---   pure Logging
---     { debugM = (liftIO . SL.debugM name) . toLog
---     , infoM  = (liftIO . SL.infoM name) . toLog
---     , warnM  = (liftIO . SL.warningM name) . toLog
---     , errorM = (liftIO . SL.errorM name) . toLog
---     }
-
 loggingForComponent :: (Applicative f, MonadIO m) => Component -> f (Logging m)
 loggingForComponent name = do
   pure Logging
-    { debugM = \_ -> pure ()
-    , infoM  = \a -> liftIO . print $ show name ++ ": " ++ toLog a
-    , warnM  = \_ -> pure ()
-    , errorM = \_ -> pure ()
+    { debugM = (liftIO . SL.debugM name) . toLog
+    , infoM  = (liftIO . SL.infoM name) . toLog
+    , warnM  = (liftIO . SL.warningM name) . toLog
+    , errorM = (liftIO . SL.errorM name) . toLog
     }
 
 newtype MakeLogging f m = MakeLogging
@@ -74,25 +65,19 @@ translateMakeLogging :: (forall a. f a -> g a) -> MakeLogging f m -> MakeLogging
 translateMakeLogging funK MakeLogging{..} =
   MakeLogging $ funK . forComponent
 
--- makeLogging :: (MonadIO f, MonadIO m) => LoggingConfig -> f (MakeLogging f m)
--- makeLogging LoggingConfig{..} = do
---   let
---     setHandler (path, level, format) = do
---       lh <- fileHandler path (fromHlogLevel level) <&> (flip setFormatter) (simpleLogFormatter format)
---       updateGlobalLogger rootLoggerName (addHandler lh)
---     overrideLevel (component, level) = updateGlobalLogger component (setLevel $ fromHlogLevel level)
---   liftIO $ updateGlobalLogger rootLoggerName (setLevel (fromHlogLevel rootLogLevel))
---   RIO.void . liftIO $ mapM setHandler fileHandlers
---   RIO.void . liftIO $ mapM overrideLevel levelOverrides
---   pure MakeLogging
---     { forComponent = loggingForComponent
---     }
-
 makeLogging :: (MonadIO f, MonadIO m) => LoggingConfig -> f (MakeLogging f m)
 makeLogging LoggingConfig{..} = do
+  let
+    setHandler (path, level, format) = do
+      lh <- fileHandler path (fromHlogLevel level) <&> (flip setFormatter) (simpleLogFormatter format)
+      updateGlobalLogger rootLoggerName (addHandler lh)
+    overrideLevel (component, level) = updateGlobalLogger component (setLevel $ fromHlogLevel level)
+  liftIO $ updateGlobalLogger rootLoggerName (setLevel (fromHlogLevel rootLogLevel))
+  RIO.void . liftIO $ mapM setHandler fileHandlers
+  RIO.void . liftIO $ mapM overrideLevel levelOverrides
   pure MakeLogging
     { forComponent = loggingForComponent
-    } 
+    }
 
 fromHlogLevel :: LogLevel -> Priority
 fromHlogLevel Debug = DEBUG
